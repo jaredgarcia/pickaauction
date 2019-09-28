4c3679e9d11b87d977e2d3f7043c0deb5c84bdd6
Allan Adan <allanadan1999@gmail.com>
2019-09-29 00:15:01 +0800

    blog.php
    blog

---
+++ blog.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   <!-- Font Awesome Icon -->
+ 		<link rel="stylesheet" href="css/font-awesome.min.css">
+
+    <title></title>
+  </head>
+  <body>
+
+  	<div style="text-align: center">
+
+  		<img src="https://images.marketleader.com/houseimages/NEOHREX/425/f_4126425.jpg" class="img-fluid shadow p-3 mb-5 bg-white rounded" alt="Responsive image" >
+
+		<h2 >Latest News</h2>
+		
+		<div style="border-bottom-width: 12px; border-bottom-color: #4A4C49 "></div>
+		<p>Blog</p>
+
+	</div>
+
+	<br /> <br />
+
+  	<div class="container">
+  		 <!-- Card -->
+	    <div class="container">  
+	    	
+	    	<h4 style="color:#8A8B8A" >FEATURED POST</h4>
+	    	
+	    
+			<!-- FEATURED POST-->
+             <div class="card shadow p-3 mb-5 bg-white rounded col-md-7" style="width: 30rem; float:left; margin-left:10px;">
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body">
+			  	<h6 class="card-subtitle mb-2 text-muted">REAL ESTATE</h6>
+			    <h4 class="card-title">Hottest Trends For Living in Tel Aviv: The Old North And Heart Of The City</h4>
+			    <h6 class="card-subtitle mb-2 text-muted">Din Golan | June 2, 2019 <br /> <a href="#" class="card-link"><u>Continue Reading</u></a></h6>
+			  </div>
+			</div>
+
+			<!-- FEATURED POST-->
+             <div class="card shadow p-3 mb-5 bg-white rounded col-md-7" style="width: 30rem; float:left; margin-left:10px;">
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body">
+			  	<h6 class="card-subtitle mb-2 text-muted">REAL ESTATE</h6>
+			    <h4 class="card-title">Hottest Trends For Living in Tel Aviv: The Old North And Heart Of The City</h4>
+			    <h6 class="card-subtitle mb-2 text-muted">Din Golan | June 2, 2019 <br /> <a href="#" class="card-link"><u>Continue Reading</u></a></h6>
+			  </div>
+			</div>
+
+			
+			
+           
+	   </div>
+
+			
+
+			<div class="container">
+			<h4 style="color:#8A8B8A; float: right; margin-right:440px;">STICKY POST</h4>
+		    <h4 style="color:#8A8B8A; float: right; margin-right: 350px;px;">LATEST POST</h4>
+			
+             <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 15rem; float:left">
+
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body">
+
+			  	<h6 class="card-subtitle mb-2 text-muted">REAL ESTATE</h6>
+			    <h4 class="card-title" style="font-size: 15px;">Hottest Trends For Living in Tel Aviv: The Old North And Heart Of The City</h4>
+			    <h6 class="card-subtitle mb-2 text-muted" style="font-size: 12px;">Din Golan | June 2, 2019 <br /> <a href="#" class="card-link"><u>Continue Reading</u></a></h6>
+			  </div>
+			</div>
+
+			 <div class="card shadow p-3 mb-5 bg-white rounded" style="width: 15rem; float:left">
+
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body">
+
+			  	<h6 class="card-subtitle mb-2 text-muted">REAL ESTATE</h6>
+			    <h4 class="card-title" style="font-size: 15px;">Hottest Trends For Living in Tel Aviv: The Old North And Heart Of The City</h4>
+			    <h6 class="card-subtitle mb-2 text-muted" style="font-size: 12px;">Din Golan | June 2, 2019 <br /> <a href="#" class="card-link"><u>Continue Reading</u></a></h6>
+			  </div>
+			</div>
+
+
+			 <!-- STICKY POST-->
+			<div class="card shadow p-3 mb-5 bg-white rounded col-md-4" style="width: 20rem;  float: right; margin-right: 100px;">
+			  <div class="card-body" style="height="100; width=100px;>
+			  	<h6 class="card-subtitle mb-2 text-muted" style="font-size: 8px;">REAL ESTATE</h6>
+			    <h4 class="card-title" style="font-size: 10px;">Hottest Trends For Living in Tel Aviv: The Old North And Heart Of The City</h4>
+			    <h6 class="card-subtitle mb-2 text-muted" style="font-size: 8px;">Din Golan | June 2, 2019 <br /> <a href="#" class="card-link"><u>Continue Reading</u></a></h6>
+			  </div>
+			</div>
+
+			<div class="card shadow p-3 mb-5 bg-white rounded col-md-4" style="width: 10rem;  float: right;">
+			  <div class="card-body" style="height="100; width=100px;>
+			  	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="..." width="67px;" height="67px;">
+			  </div>
+			</div>
+
+			 <!-- STICKY POST-->
+			<div class="card shadow p-3 mb-5 bg-white rounded col-md-4" style="width: 20rem;  float: right; margin-right: 100px; margin-top: -40px;">
+			  <div class="card-body" style="height="100; width=100px;>
+			  	<h6 class="card-subtitle mb-2 text-muted" style="font-size: 8px;">REAL ESTATE</h6>
+			    <h4 class="card-title" style="font-size: 10px;">Hottest Trends For Living in Tel Aviv: The Old North And Heart Of The City</h4>
+			    <h6 class="card-subtitle mb-2 text-muted" style="font-size: 8px;">Din Golan | June 2, 2019 <br /> <a href="#" class="card-link"><u>Continue Reading</u></a></h6>
+			  </div>
+			</div>
+
+			<div class="card shadow p-3 mb-5 bg-white rounded col-md-4" style="width: 10rem;  float: right; margin-top: -40px;">
+			  <div class="card-body" style="height="100; width=100px;>
+			  	<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="..." width="67px;" height="67px;">
+			  </div>
+			</div>
+
+
+			</div>
+
+
+
+
+	   
+
+
+	
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+ </body>
+</html>
