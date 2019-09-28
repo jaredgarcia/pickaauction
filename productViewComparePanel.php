e6e641304260ea801ab6135b170c033ab05094be
Allan Adan <allanadan1999@gmail.com>
2019-09-28 19:48:47 +0800

    adding auctionCalendar to Agent
    Calendar

---
+++ productViewComparePanel.php
@@ -0,0 +1,124 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title></title>
+  </head>
+  <body>
+    
+<!-- breadcrumb -->
+  	<div class="container-fluid" style="background-color: #808080;">
+
+  		<nav aria-label="breadcrumb">
+		  <ol class="breadcrumb" style="background-color: #808080;">
+		    <li class="breadcrumb-item active" aria-current="page" style="color: #FFFFFF;">Property Comparison</li>
+		  </ol>
+		</nav>
+
+  	</div>
+<!-- 3 rows -->
+  	<div class="container">
+	  <div class="row">
+	   
+        <!-- Card -->
+	    <div class="col-4 col-sm-4">  
+             <div class="card" style="width: 18rem;">
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body" style="background-color:#808080; ">
+			    <h5 class="card-title" style="color:#FFFFFF">Property ID: 628</h5>
+			    
+			  </div>
+			  <ul class="list-group list-group-flush">
+			    <li class="list-group-item">Price: Php 1,500,000</li>
+			    <li class="list-group-item">Location: Sta. Rosa Laguna</li>
+			    <li class="list-group-item">Property Type: House and Lot</li>
+			    <li class="list-group-item">Floor Area: 300 sqm</li>
+			    <li class="list-group-item">Bedroom: 3</li>
+			    <li class="list-group-item">Bathroom: 2</li>
+			    <li class="list-group-item">Garage: 1</li>
+			    <li class="list-group-item">Alumni Lives Near: 3</li>
+			    <li class="list-group-item">Ratings: 3 Stars</li>
+			  </ul>
+			  <div class="card-body">
+			    <a href="#" class="card-link">Card link</a>
+			    <a href="#" class="card-link">Another link</a>
+			  </div>
+			</div>
+	    </div>
+	    <!-- /Card -->
+
+	   <!-- Card -->
+	    <div class="col-4 col-sm-4">  
+             <div class="card" style="width: 18rem;">
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body" style="background-color:#808080; ">
+			    <h5 class="card-title" style="color:#FFFFFF">Property ID: 628</h5>
+			    
+			  </div>
+			  <ul class="list-group list-group-flush">
+			    <li class="list-group-item">Price: Php 1,500,000</li>
+			    <li class="list-group-item">Location: Sta. Rosa Laguna</li>
+			    <li class="list-group-item">Property Type: House and Lot</li>
+			    <li class="list-group-item">Floor Area: 300 sqm</li>
+			    <li class="list-group-item">Bedroom: 3</li>
+			    <li class="list-group-item">Bathroom: 2</li>
+			    <li class="list-group-item">Garage: 1</li>
+			    <li class="list-group-item">Alumni Lives Near: 3</li>
+			    <li class="list-group-item">Ratings: 3 Stars</li>
+			  </ul>
+			  <div class="card-body">
+			    <a href="#" class="card-link">Card link</a>
+			    <a href="#" class="card-link">Another link</a>
+			  </div>
+			</div>
+	    </div>
+	    <!-- /Card -->
+
+	    <!-- Card -->
+	    <div class="col-4 col-sm-4">  
+             <div class="card" style="width: 18rem;">
+			  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSw3nGcAYG_ZHyuvABoaqDCfVdtLhHRZY8IX93F_0k4VmepnNnp" class="card-img-top" alt="...">
+			  <div class="card-body" style="background-color:#808080; ">
+			    <h5 class="card-title" style="color:#FFFFFF">Property ID: 628</h5>
+			    
+			  </div>
+			  <ul class="list-group list-group-flush">
+			    <li class="list-group-item">Price: Php 1,500,000</li>
+			    <li class="list-group-item">Location: Sta. Rosa Laguna</li>
+			    <li class="list-group-item">Property Type: House and Lot</li>
+			    <li class="list-group-item">Floor Area: 300 sqm</li>
+			    <li class="list-group-item">Bedroom: 3</li>
+			    <li class="list-group-item">Bathroom: 2</li>
+			    <li class="list-group-item">Garage: 1</li>
+			    <li class="list-group-item">Alumni Lives Near: 3</li>
+			    <li class="list-group-item">Ratings: 3 Stars</li>
+			  </ul>
+			  <div class="card-body">
+			    <a href="#" class="card-link">Card link</a>
+			    <a href="#" class="card-link">Another link</a>
+			  </div>
+			</div>
+	    </div>
+	    <!-- /Card -->
+
+	    
+
+	    <!-- Force next columns to break to new line -->
+	    <div class="w-100"></div>
+	  </div>
+	</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
